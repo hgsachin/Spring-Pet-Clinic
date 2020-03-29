90d6cf146b9bcc8749f0ca30c81c78524b2c725c
Sachin HG <hgsachin@gmail.com>
2020-03-29 20:39:14 +0530

    Generating ID dynamically

--- pet-clinic-model/src/main/java/com/hgsachin/springpetclinic/service/map/AbstractMapService.java
+++ pet-clinic-model/src/main/java/com/hgsachin/springpetclinic/service/map/AbstractMapService.java
@@ -1,13 +1,12 @@
 package com.hgsachin.springpetclinic.service.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.hgsachin.springpetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
+import java.util.*;
 
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -17,8 +16,13 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T t) {
-        map.put(id, t);
+    T save(T t) {
+        if (t != null) {
+            if(t.getId() == null) {
+                t.setId(generateNextId());
+            }
+            map.put(t.getId(), t);
+        }
         return t;
     }
 
@@ -28,5 +32,15 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T t) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(t));
+    }
+
+    Long generateNextId() {
+        Long nextId = null;
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (Exception e) {
+            nextId = 1L;
+        }
+        return nextId;
     }
 }
--- pet-clinic-web/src/main/java/com/hgsachin/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/hgsachin/springpetclinic/bootstrap/DataLoader.java
@@ -21,13 +21,11 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("John");
         owner1.setLastName("Doe");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Reggie");
         owner2.setLastName("Johnson");
         ownerService.save(owner2);
@@ -35,13 +33,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Tom");
         vet1.setLastName("Dick");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Sam");
         vet2.setLastName("Wills");
         vetService.save(vet2);
