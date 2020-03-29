1748276c91db85306b1a61467804c1c918ddf36d
Sachin HG <hgsachin@gmail.com>
2020-03-29 13:41:41 +0530

    Owners & Vets controllers and index pages

---
+++ pet-clinic-web/src/main/java/com/hgsachin/springpetclinic/controllers/OwnersController.java
@@ -0,0 +1,13 @@
+package com.hgsachin.springpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnersController {
+
+    @RequestMapping({"owners", "owners/index"})
+    public String listOwners() {
+        return "owners/index";
+    }
+}
