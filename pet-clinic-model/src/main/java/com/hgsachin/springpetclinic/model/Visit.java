61aad01796a5fd52fdb4abd9fdbfbfd25005e629
sharnahalli <sharnahalli@sapient.com>
2020-04-06 19:03:18 +0530

    Added Project Lombok

--- pet-clinic-model/src/main/java/com/hgsachin/springpetclinic/model/Visit.java
+++ pet-clinic-model/src/main/java/com/hgsachin/springpetclinic/model/Visit.java
@@ -1,8 +1,15 @@
 package com.hgsachin.springpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "visits")
 public class Visit extends BaseEntity {
@@ -16,27 +23,4 @@ public class Visit extends BaseEntity {
     @Column(name = "description")
     private String description;
 
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public Pet getPet() {
-        return pet;
-    }
-
-    public void setPet(Pet pet) {
-        this.pet = pet;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
